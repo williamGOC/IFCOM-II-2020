4ca1d4eb4279b4a99b13e52a6e76cbe0f6e27116
William Carreras <37045478+williamGOC@users.noreply.github.com>
2020-04-21 03:44:07 -0300

    Update pybody.py

--- N-Body/pybody.py
+++ N-Body/pybody.py
@@ -12,30 +12,25 @@ from copy import copy
 
 class body(object):
 
-	def __init__(self, mass, rVec):
-		super(body, self).__init__()
-		
-		self.mass = mass
-		self.rVec = rVec
-		self.vVec = np.array([0, 0], dtype=float)
+    def __init__(self, mass, rVec):
+        super(body, self).__init__()
+	
+	self.mass = mass
+	self.rVec = rVec
+	self.vVec = np.array([0, 0], dtype=float)
 	
 
-	def __str__(self):
-		return "body object: M = {}, R = ({}, {}), V = ({}, {})".format(self.mass, 
-							self.rVec[0], self.rVec[1], self.vVec[0], self.vVec[1])
-
-
-	def distantToPoint(self, P):
-		return np.linalg.norm(P.rVec - self.rVec)
-
+    def __str__(self):
+	return "body object: M = {}, R = ({}, {}), V = ({}, {})".format(self.mass, 
+			self.rVec[0], self.rVec[1], self.vVec[0], self.vVec[1])
 	
-	def setV(self, newV):
-		self.vVec = newV
+    def setV(self, newV):
+	self.vVec = newV
 
 
-	def setR(self, newR):
-		self.rVec = newR
+    def setR(self, newR):
+	self.rVec = newR
 
 
-	def gravitationForce(self, P):
-		return (P.mass * (P.rVec - self.rVec))/self.distantToPoint(P)**3
+    def gravitationForce(self, P):
+	return (P.mass * (P.rVec - self.rVec))/np.linalg.norm(P.rVec - self.rVec)**3
